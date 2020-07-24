ff3b766f356aa25f836e4aff1a97b662dc80bdf5
waYne <wayneison@gmx.at>
2020-07-24 14:11:38 +0200

    moved up google maps interop by one folder

---
+++ Libraries/01-Shared/Elysium.Interops.Google.Maps/Service.cpp
@@ -0,0 +1,8 @@
+#include "Service.hpp"
+
+Elysium::Interop::Google::Maps::Service::Service()
+{
+}
+Elysium::Interop::Google::Maps::Service::~Service()
+{
+}
---
+++ Libraries/01-Shared/Elysium.Interops.Google.Maps/Service.hpp
@@ -0,0 +1,26 @@
+/*
+===========================================================================
+
+Copyright (C) 2017 waYne (CAM)
+
+===========================================================================
+*/
+#pragma once
+
+#ifndef ELYSIUM_INTEROP_GOOGLE_MAPS_SERVICE
+#define ELYSIUM_INTEROP_GOOGLE_MAPS_SERVICE
+
+#ifndef ELYSIUM_INTEROP_GOOGLE_MAPS_API
+#include "API.hpp"
+#endif
+
+namespace Elysium::Interop::Google::Maps
+{
+	class ELYSIUM_INTEROP_GOOGLE_MAPS_API Service final
+	{
+	public:
+		Service();
+		~Service();
+	};
+}
+#endif
